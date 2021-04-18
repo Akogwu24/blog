7867008027b901897bb3746b9e0752af3af65c79
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-04-18 21:47:10 +0100

    fake json db added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import blogData from '../BlogData';
 import BlogList from './BlogList';
 
@@ -10,10 +10,17 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log('use effect ran, render');
+    console.log(blogs);
+  }, [blogs]);
+
   return (
     <div className='home'>
       <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
     </div>
   );
 };
+
 export default Home;
